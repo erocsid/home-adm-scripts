7998d54f92d24ffd0ac744a77e8c7a5d3667b4f3
Tyler Morgan <discore@gmail.com>
2012-03-30 00:55:08 -0700

    consolidated lots of backups into this simpler file

---
+++ backup-circles.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SAMBA_MOUNTS="/maze-d"
+
+for MOUNT_POINT in $SAMBA_MOUNTS
+	do
+	if [ ! -e "$MOUNT_POINT/.mounted" ]
+		then
+		mount $MOUNT_POINT
+		sleep 5
+	fi
+	if [ ! -e "$MOUNT_POINT/.mounted" ]
+		then
+		echo $MOUNT_POINT seems to not be mounted! exiting
+		exit
+	fi
+done
+
+/usr/bin/rsync -avH --delete --ignore-errors /home/discore/. /maze-d/backups/.
+
+echo done!
